0e53d36c0ba253da957c453129543c924d512e03
lastusername <justindm1914@gmail.com>
2024-07-26 17:10:48 -0400

    Added a JS file that displays text to the webpage.

--- JavaScript_1/index.js
+++ JavaScript_1/index.js
@@ -1,13 +1,20 @@
 function greet(fname, lname) {
     console.log("Hello "+fname+" "+lname)
 }
-
+function disSum() {
+    let integer2 = 3;
+    let sum = (integer + integer2);
+    window.alert("The sum of my integers is "+ sum);
+}
 console.log("hello");
 
 let inp = prompt("hello user, type something below!");
 let integer = 2;
-let isTrue = true;
-let isNotFalse = true;
+
+
+
+var isTrue = true;
+var isNotFalse = true;
 
 if (isTrue === true) {
     console.log(" '"+inp+"' is what inputed and the integer I chose is "+integer);
@@ -15,13 +22,13 @@ if (isTrue === true) {
     console.log("How is that possible");
 }
  
-while (isTrue === true ||isNotFalse === true ){
+while (isTrue === true && isNotFalse === true ){
     let usr_Int = parseInt(prompt("lets play guess the number. choose a number (hint, check console)"));
     if (usr_Int != 2) {
         window.alert("you got it wrong, try again");
 }   else {
      window.alert("you got it right! now we will proceed");
-     endlooop;
+     var isTrue = false;
     }
     
 }
