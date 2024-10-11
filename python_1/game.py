c7125726ef1e760318db274cb548413fdf8459f4
lastusername <justindm1914@gmail.com>
2024-10-11 01:54:15 -0400

    Finished main components Created a function that checks if user wants to continue after making a mistake imported random created second while loop that determines if the user guess the number correctly

--- python_1/game.py
+++ python_1/game.py
@@ -1,15 +1,38 @@
+import random
+
+def tryAgain():
+    UsrChoice = input("An error occured with your input, would you like to try again?")
+    if UsrChoice.lower() == "y":
+            print("Alright!")
+    else:
+            print("Alright")
+            Valid = False
+
+
+
+
+
+
 Valid = True
 while (Valid):
     try:
         highVar = int(input("What is your high variable?"))
-        lowVar = int(input("What is your lwo variable?"))
+        lowVar = int(input("What is your low variable?"))
+        Valid = False
     except:
-        UsrChoice = int(input("An error occured with your inputs, would you like to try again?"))
-        if UsrChoice.lower() == "y":
-            print("Alright!")
-        else:
-            print("Alright")
-            Valid = False
+        tryAgain()
 botNum = random.randint(lowVar, highVar)
 
-input
+Valid = True
+while (Valid):
+    try:
+        Var = int(input("Guess a number between " + str(lowVar) + " through " + str(highVar) + "?"))
+        Valid = False
+        if Var == botNum:
+            print("You got it!")
+        else:
+            print("You didn't get it, try again")
+            Valid = True
+    except:
+        tryAgain()
+    
