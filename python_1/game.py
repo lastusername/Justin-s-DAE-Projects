0feecb7fffeafc60707f173d07e563eb6ef57d2d
lastusername <justindm1914@gmail.com>
2024-10-12 20:52:04 -0400

    Updated process of program created a function for the the main component of game.py Fixed the process of playing again when the user agrees to called the the function that runs the main component of game.py into Main.py

--- python_1/game.py
+++ python_1/game.py
@@ -1,34 +1,38 @@
 import random
 
 def YesNo(UsrChoice):
+    Valid = True
     if UsrChoice.lower() == "y":
             print("Alright!")
+            return Valid
     else:
             print("Alright")
             Valid = False
-Valid = True
-while (Valid):
-    try:
-        lowVar = int(input("What is your low variable?"))
-        highVar = int(input("What is your high variable?"))
-        botNum = random.randint(lowVar, highVar)
-        Valid = False
-    except:
-        UsrChoice = input("An error occured with your input, would you like to try again?")
-        YesNo(UsrChoice)
+            return Valid
+def game():
+    Valid = True
+    while (Valid):
+        try:
+            lowVar = int(input("What is your low variable?"))
+            highVar = int(input("What is your high variable?"))
+            botNum = random.randint(lowVar, highVar)
+            Valid = False
+        except:
+            UsrChoice = input("An error occured with your input, would you like to try again?")
+            YesNo(UsrChoice)
 
 
-Valid = True
-while (Valid):
-    try:
-        Var = int(input("Guess a number between " + str(lowVar) + " through " + str(highVar) + "?"))
-        Valid = False
-        if Var == botNum:
-            print("You got it!")
-        else:
-            print("You didn't get it, try again")
-            Valid = True
-    except:
-        UsrChoice = input("An error occured with your input, would you like to try again?")
-        YesNo(UsrChoice)
+    Valid = True
+    while (Valid):
+        try:
+            Var = int(input("Guess a number between " + str(lowVar) + " through " + str(highVar) + "?"))
+            Valid = False
+            if Var == botNum:
+                print("You got it!")
+            else:
+                print("You didn't get it, try again")
+                Valid = True
+        except:
+            UsrChoice = input("An error occured with your input, would you like to try again?")
+            YesNo(UsrChoice)
     
