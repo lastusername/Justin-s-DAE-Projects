e55d17b4d62e8a9fa39e2cc2c9129ca6142b2ff2
lastusername <justindm1914@gmail.com>
2024-12-07 00:34:19 -0500

    installed firebase

---
+++ firebase_1/FirebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDBBj5GkausRU9cBGWFNm2vR96WSuFSjMs",
+  authDomain: "fir-1-db-155c4.firebaseapp.com",
+  projectId: "fir-1-db-155c4",
+  storageBucket: "fir-1-db-155c4.firebasestorage.app",
+  messagingSenderId: "734383330415",
+  appId: "1:734383330415:web:388eead8f0c01c69dca99a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
