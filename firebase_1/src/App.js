888a1b0dec5a999452544e0fd21fe27f8f50eccc
lastusername <justindm1914@gmail.com>
2024-12-07 23:38:27 -0500

    Used Sign in and created sign up component Attempted to use useState to switch between sign in and sign up component

--- firebase_1/src/App.js
+++ firebase_1/src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import SignIn from './components/SignIn.js'
+import {useState} from "react";
+import SignUp from './components/SignUp.js';
 
 function App() {
+  let [signUp, setSignUp] = useState(<SignIn />)
+
+  function Register() {
+    return setSignUp ((<SignUp />));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   {signUp}
+   <p className="mt-10 text-center text-sm/6 text-gray-500">
+       Not a member?{' '}
+          <button onclick={Register}><a className="font-semibold text-indigo-600 hover:text-indigo-500">
+            Start a 14 day free trial
+          </a>
+          </button>
+            </p>
     </div>
   );
 }
