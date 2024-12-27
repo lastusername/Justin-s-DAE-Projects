44ad89c9fe9702a4dff2354e60f89dd1f3c02451
lastusername <justindm1914@gmail.com>
2024-12-26 21:28:23 -0500

    created new Application: Post

--- django_1/django_1/settings.py
+++ django_1/django_1/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myApp',
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'django_1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['../myApp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
