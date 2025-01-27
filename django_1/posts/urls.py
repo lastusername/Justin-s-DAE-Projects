fdc2b5ada5bb3fbc204bb9685b155cb70e81d304
lastusername <justindm1914@gmail.com>
2025-01-27 08:22:31 -0500

    created more post using the shell, sorted post by newsest date, began developing indivisual post page backend

--- django_1/posts/urls.py
+++ django_1/posts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.posts_list),
+    path("", views.posts_list, name='posts'),
+    path("<slug:slug>", views.post_post_page, name='posts'),
 ]
