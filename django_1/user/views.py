08fef2ffbb0ba8bb8b253ad76f163f18926249da
Justin <justin@Justins-MacBook-Pro.local>
2025-04-17 17:28:14 -0400

    Finalyzedfinalize the login page backend

--- django_1/user/views.py
+++ django_1/user/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-
+from django.contrib.auth import login
 # Create your views here.
 def registration(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            login(request, form.save())
             return redirect("posts:list")
     else:
         form = UserCreationForm()
@@ -14,7 +14,10 @@ def registration(request):
 
 def signIn(request):
     if request.method == "POST":
-        return redirect("user:login")
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect("posts:list")
     else:
         form = AuthenticationForm(request.POST)
         return render(request, 'users/login_page.html', {"form": form})
